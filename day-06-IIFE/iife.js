379ab10edfd175bcca544398796830a380adb337
Chepkemboi <Chepkemboirabecca@gmail.com>
2023-06-09 21:11:51 +0300

    added js IIFE

---
+++ day-06-IIFE/iife.js
@@ -0,0 +1,15 @@
+
+// Create a counter using an IIFE. The counter should start at 1 and increment by 1 each time it is called.
+// The counter value should be logged to the console.
+
+
+// Write an IIFE that accepts an array of numbers as an argument and returns the average of those numbers.
+
+
+// Implement a module using an IIFE that exposes a public method to calculate the 
+// factorial of a given number. The module should also have a private variable 
+// to keep track of the number of times the factorial method is called.
+
+
+// Create a function using an IIFE that generates a random number between 1 and 10, inclusive,
+// each time it is called. The generated random number should be logged to the console.
